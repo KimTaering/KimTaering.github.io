8f5810718bb15a16d19fa38ec1387f17cfec15bd
KimTaerring <92096892+KimTaering@users.noreply.github.com>
2024-04-07 22:43:10 +0900

    add post

---
+++ _posts/2024-04-07-linux-basic-command.md
@@ -0,0 +1,7 @@
+---
+title: 리눅스 기본 명령어
+author: KimTaering
+# date: 2019-08-11 00:34:00 +0800
+# categories: [Blogging, Tutorial]
+# tags: [linux]
+---
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712496895',resources: [
+    cacheName: 'chirpy-1712497375',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
