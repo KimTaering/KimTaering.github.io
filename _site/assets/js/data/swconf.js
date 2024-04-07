75ec469b02166fdcebedb401cd92a83811d3a729
KimTaerring <92096892+KimTaering@users.noreply.github.com>
2024-04-07 22:40:18 +0900

    Change title

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712495874',resources: [
+    cacheName: 'chirpy-1712496895',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
